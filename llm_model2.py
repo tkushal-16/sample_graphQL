2b1b084faf7114e6189b4b41e17142f91f782d50
tkushal-16 <kushaltkanaiaha7777@gmail.com>
2026-03-17 14:21:33 +0530

    rename file and add example.env file

--- llm_model2.py
+++ llm_model2.py
@@ -1,3 +1,11 @@
+"""The LLM model details:
+Model name :-   OpenAI: gpt-oss-120b 
+Model Description :- gpt-oss-120b is an open-weight, 117B-parameter Mixture-of-Experts (MoE) language model 
+from OpenAI designed for high-reasoning, agentic, and general-purpose production use cases. 
+It activates 5.1B parameters per forward pass and is optimized to run on a single H100 GPU with native MXFP4 quantization. 
+The model supports configurable reasoning depth, full chain-of-thought access, and native tool use, including function calling,
+ browsing, and structured output generation."""
+
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
